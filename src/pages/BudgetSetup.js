2e6c2f2c2754b93cec31d4f9973dec27458294e5
Hayley K <mrs.kornilenko@gmail.com>
2018-11-08 20:38:40 -0500

    Fixed up some files and folders

---
+++ src/pages/BudgetSetup.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+// import Jumbotron from '../../components/Jumbotron';
+class BudgetSetup extends Component {
+  render() {
+    return <h1> this is a the budget setup</h1>;
+  }
+}
+
+export default BudgetSetup;
---
+++ src/pages/Debt.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+// import Jumbotron from '../../components/Jumbotron';
+class Debt extends Component {
+  render() {
+    return <h1> this is a the debt page</h1>;
+  }
+}
+
+export default Debt;
