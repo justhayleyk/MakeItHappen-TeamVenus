8e1a82fa53bead7fd62527cf4fcb55c1c47667fb
Hayley K <mrs.kornilenko@gmail.com>
2018-11-08 19:14:46 -0500

    reactStrap init

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'reactstrap';
 
 class App extends Component {
   render() {
@@ -8,6 +9,7 @@ class App extends Component {
       <div className="App">
         <h1>MAKE IT HAPPEN</h1>
         <h3>Work In Progress</h3>
+        <button>It Work's</button>
       </div>
     );
   }
